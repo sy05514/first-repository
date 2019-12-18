5bfcd100319e45138241ef292971ccf283efcb94
wujc <237977800>
2019-12-18 16:52:40 +0800

    二维码生成

---
+++ qrcode/qrcodeDemo.py
@@ -0,0 +1,12 @@
+import qrcode
+qr = qrcode.QRCode(
+	version=2,
+	error_correction=qrcode.constants.ERROR_CORRECT_L,
+	box_size=10,
+	border=1
+)
+#设置二维码的大小
+qr.add_data("https://blog.csdn.net/caidewei121/")
+qr.make(fit=True)
+img = qr.make_image()
+img.save("my_blog.png")
