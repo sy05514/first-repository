dfa0d26defb105f3ba92fd8dae8371375a8f966f
wujc <237977800>
2019-12-20 10:18:04 +0800

    12.19学习demo

---
+++ pyqt5/2019.12.20/main.py
@@ -0,0 +1,12 @@
+import sys
+import demo
+
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    ui = demo.Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
