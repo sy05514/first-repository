6b56c28812a82ae4cf171fdd1896fff07c3a93c9
wujc <237977800>
2019-12-24 17:35:03 +0800

    计算器部分逻辑

--- 计算器(继承重写)/Main.py
+++ 计算器(继承重写)/Main.py
@@ -2,14 +2,41 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import GuiCacular, sys
 
 
-class myDialog(GuiCacular.Ui_MainWindow):  # 继承自UI_Diglog类，注意我把UI_Dialog放在了untitled.py中，如果你把这个类放在了XXX.py文件中，就应该是XXX.UI_Dialog
+class myDialog(GuiCacular.Ui_MainWindow):
+
+    lastClickType = 'number'
+
     def __init__(self, Dialog):
         super().setupUi(Dialog)  # 调用父类的setupUI函数
-        self.pushButton_13.clicked.connect(self.helloworld)  # 将按钮点击事件和helloworld函数绑定
+        self.pushButton.clicked.connect(lambda: self.clickNum('7'))
+        self.pushButton_2.clicked.connect(lambda: self.clickNum('8'))
+        self.pushButton_3.clicked.connect(lambda: self.clickNum('9'))
+        self.pushButton_4.clicked.connect(lambda: self.clickNum('4'))
+        self.pushButton_5.clicked.connect(lambda: self.clickNum('5'))
+        self.pushButton_6.clicked.connect(lambda: self.clickNum('6'))
+        self.pushButton_7.clicked.connect(lambda: self.clickNum('1'))
+        self.pushButton_8.clicked.connect(lambda: self.clickNum('2'))
+        self.pushButton_9.clicked.connect(lambda: self.clickNum('3'))
+        self.pushButton_10.clicked.connect(lambda: self.clickNum('0'))
+        self.pushButton_17.clicked.connect(self.clickClear)
 
-    def helloworld(self, i):  # label上显示文字hello world
-        print('hello')
+    # 按键为数字时
+    def clickNum(self, index):
+        if len(self.label.text()) > 10:
+            print('数据过长，请清理后重试')
+            return
+        if self.lastClickType == 'number':
+            if self.label.text() == '0':
+                self.label.setText(index)
+            else:
+                self.label.setText(self.label.text() + index)
+        elif self.lastClickType == '+':
+            self.label.setText(self.label.text() + index)
 
+    # 清空按钮
+    def clickClear(self):
+        self.lastClickType = 'number'
+        self.label.setText('0')
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
