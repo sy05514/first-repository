3e3e475046236e452b8f78b799bc00bf72b4e625
wujc <237977800>
2019-12-24 10:54:25 +0800

    退出应用程序

--- pyqt5/代码UI设计/2019.12.23/demo.py
+++ pyqt5/代码UI设计/2019.12.23/demo.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QDesktopWidget,QMainWindow,QApplication
+from PyQt5.QtWidgets import QDesktopWidget,QMainWindow,QApplication,QHBoxLayout,QWidget,QPushButton
 from PyQt5.QtGui import QIcon
 
 class Demo(QMainWindow):
@@ -12,10 +12,24 @@ class Demo(QMainWindow):
         # 设置窗口尺寸
         self.resize(400,300)
 
+        # 添加Button
+        self.button = QPushButton('退出应用程序')
+        self.button.clicked.connect(self.onClick_Button)
+
+        # 创建水平布局
+        layout = QHBoxLayout()
+        layout.addWidget(self.button)
+
+        mainFrame = QWidget()
+        mainFrame.setLayout(layout)
+
+        self.setCentralWidget(mainFrame)
+
         self.status = self.statusBar()
         # 设置消息
         self.status.showMessage('只存在5秒的消息', 5000)
 
+    # 设置窗口位置
     def center(self):
         # 获取屏幕坐标
         screen = QDesktopWidget().screenGeometry()
@@ -25,6 +39,14 @@ class Demo(QMainWindow):
         newTop = (screen.height()-size.height())/2
         self.move(newLeft, newTop)
 
+    # 按钮单击事件的方法
+    def onClick_Button(self):
+        sender = self.sender()
+        print(sender.text() + '按钮被按下')
+        app = QApplication.instance()
+        # 退出应用程序
+        app.quit()
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     # 设置图标
@@ -32,6 +54,7 @@ if __name__ == '__main__':
 
     main = Demo()
     main.center()
+
     main.show()
 
     sys.exit(app.exec_())
