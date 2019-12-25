95fdf3683b349e07721ae6fc9d920a4125815870
wujc <237977800>
2019-12-25 17:00:16 +0800

    pyqt打开txt文件并按格式读取文本信息，成功失败均在状态栏上显示

---
+++ open-read-txt/Main.py
@@ -0,0 +1,44 @@
+import os
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
+
+import ReadTxt, sys
+
+userArray = []
+accountArray = []
+
+class myDialog(ReadTxt.Ui_MainWindow):
+
+    def __init__(self, Dialog):
+        super().setupUi(Dialog)  # 调用父类的setupUI函数
+        self.toolButton.clicked.connect(lambda: self.openFile())
+
+    # 打开文件，存储用户信息
+    def openFile(self):
+        filepath, filetype = QFileDialog.getOpenFileName(filter="*.txt")
+        if not filepath:
+            return
+        # 清空存储用户信息
+        userArray = []
+        accountArray = []
+        self.statusbar.showMessage('')
+        with open(filepath) as f:
+            infoArr = f.read().split()
+            if len(infoArr) % 2 != 0:
+                self.statusbar.showMessage('当前文档格式有误')
+                return
+            for index, val in enumerate(infoArr):
+                if index % 2 == 0:
+                    userArray.append(val)
+                else:
+                    accountArray.append(val)
+        self.statusbar.showMessage('文件载入成功')
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = myDialog(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ open-read-txt/ReadTxt.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ReadTxt.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(4000, 2000)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.toolButton = QtWidgets.QToolButton(self.centralwidget)
+        self.toolButton.setGeometry(QtCore.QRect(580, 60, 31, 20))
+        self.toolButton.setObjectName("toolButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "小工具"))
+        self.toolButton.setText(_translate("MainWindow", "..."))
